44cd82db600f507953bb7a2a76c70feae2c74db3
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2023-09-26 23:06:10 +0200

    wdio-3-lection

--- src/tests/first.test.ts
+++ src/tests/first.test.ts
@@ -98,21 +98,68 @@ describe("Login page tests", () => {
   - логаут
   - проверка страницы куда вернулись
   */
-  it.only("fgdhgd", async () => {
+  it("fgdhgd", async () => {
     const links = await $$("ul a");
-    // for (const link of links) {
-    //   const text = await link.getText();
-    //   console.log(text);
-    // }
-    // links.forEach(async (link) => console.log(await link.getText()));
-    // const linksTextArray = await Promise.all(links.map(async (link) => await link.getText()));
-    // console.log(linksTextArray);
-    const linkToAuthorizationPage = links.find(async (link) => {
+    let resultLink;
+    for (const link of links) {
       const text = await link.getText();
-      console.log(text);
-      return text === "Form Authentication";
-    });
-    console.log(await linkToAuthorizationPage?.getText());
-    await linkToAuthorizationPage?.click();
+      if (text === "Form Authentication") {
+        resultLink = link;
+        break;
+      }
+    }
+    await resultLink?.click();
+    await browser.pause(5000);
+  });
+
+  it("wait", async () => {
+    await browser.url("https://the-internet.herokuapp.com/dynamic_loading");
+    await $("h3").waitForDisplayed();
+    await $(`a[href="/dynamic_loading/1"]`).click();
+    const btn = $("div#start > button");
+    await btn.waitForDisplayed({ timeout: 5000 });
+    await btn.click();
+    // await $("#loading").waitForDisplayed({ reverse: true, timeout: 10000, interval: 200 });
+    // const finish = await $("div#finish");
+    // await finish.waitForDisplayed();
+    // expect(await finish.isDisplayed()).toBe(true);
+
+    await browser.waitUntil(
+      async () => {
+        const finish = await $("div#finish h4");
+        const text = await finish.getText();
+        const isDisplayed = await finish.isDisplayed();
+        const isEnabled = await finish.isEnabled();
+        return isDisplayed && text === "Hello World!" && isEnabled;
+      },
+      {
+        timeout: 8000,
+        timeoutMsg: "Finish element is not displayed or message is incorrenct",
+      }
+    );
+
+    /*
+    Pooling - повторяющийся опрос определенного ресурса на предмет определенных данных с определенными интервалами
+    Interval - время между опросами 
+    Timeout - максимальное время проведения pooling-а
+    */
+
+    /*
+    Auto-wait conditions:
+    - Existence: WebDriverIO ожидает существования элемента в DOM.
+    - Visibility: он ожидает, пока элемент станет видимым (не будет скрыт CSS или скрыт другими элементами).
+    - Interactability: он ожидает, пока элемент станет интерактивным, то есть он не отключен и не закрыт другими элементами.
+    */
+  });
+
+  it.only("Chaining", async () => {
+    await $(`h1.heading`).waitForDisplayed({ timeout: 5000, timeoutMsg: `Home page not opened after 5 seconds`, reverse: false });
+    await $(`a[href="/login"]`).click();
+    await $(`//h2[text()="Login Page"]`).waitForDisplayed({ timeout: 5000, timeoutMsg: `Login page not opened after 5 seconds`, reverse: false });
+    const form = await $('form[name="login"]');
+    await form.$("#username").setValue("tomsmith");
+    await form.$("#password").setValue("SuperSecretPassword!");
+    const formMethod = await form.getAttribute("method");
+    expect(formMethod).toBe("post");
   });
 });
